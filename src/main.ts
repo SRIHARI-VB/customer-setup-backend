927c6802ea53748b0798a4590fa90f821be05dc3
SRIHARI-VB <srihari.vb.kovai@gmail.com>
2023-10-30 12:28:09 +0530

    backend completed - need to deploy in amplify

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  })
+  const cors = require('cors');
+  app.use(cors({origin: '*'}));
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('The Median API description')
@@ -19,6 +14,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3001);
+  await app.listen(3002);
 }
 bootstrap();
